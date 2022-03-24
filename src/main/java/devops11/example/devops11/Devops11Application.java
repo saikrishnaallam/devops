a02db0a8a56cdd10caf409cb061622189197d57c
saikrishnaallam <sunny.sai123@gmail.com>
2022-03-24 13:46:12 -0400

    adding new maven Java11 files

---
+++ src/main/java/devops11/example/devops11/Devops11Application.java
@@ -0,0 +1,13 @@
+package devops11.example.devops11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devops11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devops11Application.class, args);
+	}
+
+}
---
+++ src/test/java/devops11/example/devops11/Devops11ApplicationTests.java
@@ -0,0 +1,13 @@
+package devops11.example.devops11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devops11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
